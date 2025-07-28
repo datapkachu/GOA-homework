b947f30422cd741698b9addbf01228913aa37a25
datapkachu <makasakandelidze@gmail.com>
2025-07-28 18:22:12 +0400

    1

---
+++ day08/homework/data.py
@@ -0,0 +1,63 @@
+# print(10==45)
+# print(13>43)
+# print(90<23)
+# print(120==23)
+# print(12>76)
+# print(8==8)
+# print(5>2)
+# print(8<45)
+# print(45>12)
+# print(7==7)
+
+
+# iteration არის მოქმედების გამეორება
+# sequancing არის თანმიმდევრობა
+# selection არის არჩევა
+
+
+# cool=15
+# cool=234
+# print(cool)
+# ეს არის sequancing იმიტომ რომ ცვლადი cool განახლდა თანმიმდევრობის მიხედვით დაიპრინტება 234
+
+
+# for loop არის რაიმის გამეორება ის გვეხმარება ბევრი ერთნაირი კოდის ჩქარა დასაწერად
+
+
+# rane() ფუნქციაში უნდა ჩაწერო თუ რამდენჯერ გინდა კოდის გამეორე
+
+
+# for loop in range(100):
+#     print("საკანდელიძე")
+
+
+# for i in range(46):
+#     print("white")
+
+
+# for i in range(32):
+#     print("d")
+
+
+# a=(input("what colour is the sky?:"))
+# b=(input("whats ur name?:"))
+# c=(input("is ur dog cool?:"))
+# d=(str(input("enter ur age:")))
+# print(a+b+c+d)
+
+
+# e=False
+# f=15
+# g=4.7
+# h="not cool"
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+
+
+# i=(int(input("1st number")))
+# j=(int(input("2nd number")))
+# k=(int(input("3rd number")))
+# l=(int(input("4th number")))
+# print(i+j+k+l)
