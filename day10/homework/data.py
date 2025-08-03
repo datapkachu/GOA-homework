84867f90a79f7bca1dc8683b4439bbbafe576517
datapkachu <makasakandelidze@gmail.com>
2025-08-03 17:36:58 +0400

    1

---
+++ day10/homework/data.py
@@ -0,0 +1,74 @@
+# N1
+# for i in range(39):
+#     print(i)
+
+# N2
+# for i in range(30,40):
+#     print(i)
+
+# N3
+# for i in range(12,120):
+#     print(i)
+
+# N4
+# for i in range(18):
+#     print("data")
+#     print(15)
+
+# N5
+# for i in range(10,100,3):
+#     print(i)
+
+# N6
+# for i in range(1,31):
+#     print(str(i) + "ვაშლი")
+
+# N7
+# for i in range(13):
+#     input("enter ur age: ")
+
+# N8
+# i=0
+# while i < 10:
+#     print("data")
+#     i = i + 1
+
+# N9
+# i = 0
+# while i < 24:
+#     print(i)
+#     i = i + 1
+
+
+# N10
+# for i in range(0,500,25):
+#     print("makaroni")
+
+# i = 15
+# while i > 0:
+#     print(i)
+#     i = i - 1
+
+# for i in range(9,451,9):
+#     print(str(i) + "ქულა")
+
+# i = 15
+# while i < 1081:
+#     print(str(i) + " გრადუსი ")
+#     i = i + 15
+
+# for i in range(700,1400,70):
+#     print(i)
+
+# i = 2
+# while i < 20000000:
+#     print(i)
+#     i = i * 10
+
+# for i in range(1,102,5):
+#     print("ფული =" + str(i) + "ლარი")
+
+# i = 8000
+# while i > 31.24:
+#     print(i)
+#     i = i / 2
