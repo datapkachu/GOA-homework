968ff6128a4eebb2f1820c17a977d1e00d90b1a2
datapkachu <makasakandelidze@gmail.com>
2025-08-11 17:52:09 +0400

    1

---
+++ day12/homework/data.py
@@ -0,0 +1,25 @@
+# conditional statement ნიშნავს პირობით განცხადებებს, მასში შედის if და else
+
+
+for i in range(50):
+    print("hello world")
+
+
+i=3
+while i < 18:
+    print(i)
+    i = i+1
+
+
+password=int(input("enter correct password:"))
+if password==1234:
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+
+animal=input("რა არის შენი საყვარელი ცხოველი:")
+if animal=="ძაღლი":
+    print("woaf! woaf!")
+else:
+    print("შენ არ გყავს ძაღლი")
