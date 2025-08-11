f81c20aa81d67954794281e25fa5c30a95112a3a
datapkachu <makasakandelidze@gmail.com>
2025-08-11 17:23:57 +0400

    1

---
+++ day12/classwork/data.py
@@ -0,0 +1,18 @@
+for i in range(1,11):
+    print(i)
+
+
+i = 5
+while i < 21:
+    print(i)
+    i = i+1
+
+
+# conditional statement არის if და else ის გამოიყენება პირობების შესადგენათ
+
+
+age = input("how old are u?")
+if age == 18:
+    print("you are an adult")
+else:
+    print("you are a kid")
