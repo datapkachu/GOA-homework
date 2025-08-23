9babdb5b36324823e305eaf25de52aaef7d9a3ef
datapkachu <makasakandelidze@gmail.com>
2025-08-23 13:44:01 +0400

    1

---
+++ day16/homework/data.py
@@ -0,0 +1,33 @@
+a=324
+if a>10:
+    print("more than 10")
+else:
+    print("less than 10")
+
+
+b=int(input("pick a number from 1 to 15:"))
+if b==15:
+    print("equal to 15")
+else:
+    print("not equal to 15")
+
+
+c=input("who am i?:")
+if c=="giorgi":
+    print("you are correct")
+else:
+    print("you are wrong")
+
+
+for i in range(50,100,5):
+    print(i)
+
+
+for i in range(1):
+    print("დათა საკანდელიძე")
+
+
+i=20
+while i<50:
+    print(i)
+    i=i+1
