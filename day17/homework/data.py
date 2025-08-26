89c5f82152c9fbbac964d2c152f6d7f34105aeea
datapkachu <makasakandelidze@gmail.com>
2025-08-26 14:26:22 +0400

    1

---
+++ day17/homework/data.py
@@ -0,0 +1,30 @@
+# a=int(input("შემოიყვანეთ რაიმე რიცხვი:"))
+# if a == 0:
+#     print("ეს რიცხვი ნულია")
+# elif a < 0:
+#     print("ეს რიცხვი უარყოფითია")
+# elif a > 0:
+#     print("ეს რიცხვი დადებითია")
+
+
+# b=int(input("შეიყვანეთ თქვენი ასაკი:"))
+# if b > 120:
+#     print("გურუ ანდ ჯადოქარი")
+# elif b == 0 or 13 > b > 0 :
+#     print("ბავსვი ხარ")
+# elif 20 > b > 12 :
+#     print("მოზარდი/თინეიჯერი ხართ")
+# elif 65 > b > 19:
+#     print("ზრდასრული ხართ")
+# elif 121 > b > 64:
+#     print("ხანში შესული ხართ")
+# if b < 0:
+#     print("არასწორი ინფო")
+
+
+c = "ah_hell_nah"
+if c == "ah_hell_nah":
+    print("password is correct")
+else:
+    print("password is incorrect, please try again")
+    input("enter password:")
