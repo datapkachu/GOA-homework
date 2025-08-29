4545684fff7ee549f26f1f293ddac81808cd56f3
datapkachu <makasakandelidze@gmail.com>
2025-08-29 18:59:02 +0400

    1

--- day16/homework/data.py
+++ day16/homework/data.py
@@ -1,33 +1,62 @@
-a=324
-if a>10:
-    print("more than 10")
-else:
-    print("less than 10")
+# a=int(input("შემოიყვანეთ რაიმე რიცხვი:"))
+# if a == 0:
+#     print("ეს რიცხვი ნულია")
+# elif a < 0:
+#     print("ეს რიცხვი უარყოფითია")
+# elif a > 0:
+#     print("ეს რიცხვი დადებითია")
 
 
-b=int(input("pick a number from 1 to 15:"))
-if b==15:
-    print("equal to 15")
-else:
-    print("not equal to 15")
+# b=int(input("შეიყვანეთ თქვენი ასაკი:"))
+# if b > 120:
+#     print("გურუ ანდ ჯადოქარი")
+# elif b == 0 or 13 > b > 0 :
+#     print("ბავსვი ხარ")
+# elif 20 > b > 12 :
+#     print("მოზარდი/თინეიჯერი ხართ")
+# elif 65 > b > 19:
+#     print("ზრდასრული ხართ")
+# elif 121 > b > 64:
+#     print("ხანში შესული ხართ")
+# elif b < 0:
+#     print("არასწორი ინფო")
 
 
-c=input("who am i?:")
-if c=="giorgi":
-    print("you are correct")
-else:
-    print("you are wrong")
+# c=input("guess my password:")
+# while c != "123":
+#     c=input("guess my password:")
+#     if c == "123":
+#         print("password is correct, it took u many attempts")
+#     elif c == "nah strong password":
+#         print("bud gave up")
+#         break
 
 
-for i in range(50,100,5):
-    print(i)
+# d=float(input("რიცხვი1:"))
+# e=float(input("რიცხვი2:"))
+# f=float(input("რიცხვი3:"))
+# if f < d > e :
+#     print(d)
+# elif d < f > e:
+#     print(f)
+# elif f < e > d:
+#     print(e)
 
 
-for i in range(1):
-    print("დათა საკანდელიძე")
-
-
-i=20
-while i<50:
-    print(i)
-    i=i+1
+# g=int(input("შემოიტანეთ რიცხვი 1 დან 7 ის ჩათვლით:"))
+# if g == 1:
+#     print("ორშაბათ")
+# elif g == 2:
+#     print("სამშაბათი")
+# elif g == 3:
+#     print("ოთხშაბათი")
+# elif g == 4:
+#     print("ხუთშაბათი")
+# elif g == 5:
+#     print("პარასკევი")
+# elif g == 6:
+#     print("შაბათი")
+# elif g == 7:
+#     print("კვირა")
+# else:
+#     print("არ ვიცი ეგ რა დღეა")
