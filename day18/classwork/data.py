8e8636cb102cc9d634e808c8c34a0399312d5ac1
datapkachu <makasakandelidze@gmail.com>
2025-09-01 17:29:16 +0400

    1

---
+++ day18/classwork/data.py
@@ -0,0 +1,33 @@
+# a=str(input("1st number:"))
+# b=str(input("2nd number:"))
+# c=str(input("3rd number:"))
+# if a==b:
+#     print("1 and 2 are equal")
+# elif a==c:
+#     print("1 and 3 are equal")
+# elif b==c:
+#     print("2 and 3 are equal")
+# elif a==b==c:
+#     print("all numbers are equeal to each other")
+# elif a!=b!=c:
+#     print("nothing is equal")
+
+
+# d=str(input("choose a numbver between 1 to 12:"))
+# if d==12 or d==1 or d==2:
+#     print("winter")
+# elif d==3 or d==4 or d==5:
+#     print("spring")
+# elif d==6 or d==7 or d==8:
+#     print("summer")
+# elif d==9 or d==10 or d==11:
+#     print("autumn")
+
+
+e=input("enter your name:")
+if e=="admin:":
+    f=input("enter your password:")
+    if f=="adminpassword123":
+        print("hi admin")
+else:
+    print("hi user")
