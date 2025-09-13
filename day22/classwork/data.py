890b51245c26d91046b6f3e53382d3bc4702a60b
datapkachu <makasakandelidze@gmail.com>
2025-09-13 22:54:30 +0400

    1

---
+++ day22/classwork/data.py
@@ -0,0 +1,18 @@
+# list = ["data","nika","luka","dachi","saba","gio","zura","goga","jora","sandro"]
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+# print(list[6])
+# print(list[7])
+# print(list[8])
+# print(list[9])
+
+
+list2 = ["badrijani","gogra","xaxvi","kitri","kartofili","nirori"]
+list2[0] = "viri"
+list2[2] = "tagvi"
+list2[4] = "bokveri"
+print(list2)
