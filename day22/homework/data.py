1e3c235ee94844926665c450f7e2a1795fcb8868
datapkachu <makasakandelidze@gmail.com>
2025-09-16 16:50:32 +0400

    1

---
+++ day22/homework/data.py
@@ -0,0 +1,93 @@
+# index არის list-ში ჩაწერილი მნიშვნელობეი
+
+
+# a=[4,6,1,5,9,8,4]
+# print(a[0]+a[1])
+# print(-a[0]+a[1])
+# print(a[3]+a[4])
+# print(a[0]*a[3])
+# print(a[0]*a[2])
+# print(a[1]+a[2])
+# print("27 ვერ მიიღებ ამ ციფრებით")
+
+
+# b=["data","joora","zura","nika","luka","dachi","irakli","goga","sandro","andria"]
+# print(b[5])
+# print(b[9])
+# print(b[3])
+# print(b[7])
+
+
+# c=["ჩიფსი","კოკაკოლა","ბურგერი","ფანტა","კარტოფილი"]
+# i=0
+# while i < 5:
+#     print(c[i])
+#     i+=1
+
+# e=["ჩიფსი","კოკაკოლა","ბურგერი","ფანტა","კარტოფილი"]
+# for i in range(5):
+#     print(e[i])
+
+
+# f=["ice cream","cake","chocolate","puding","cheese cake","candy","milk shake"]
+# f[3]="vashli"
+# f[6]="msxali"
+# f[4]="atami"
+# print(f)
+
+
+# True and False or False and True or False and False or True ---> True
+
+
+# g=["lion","cat","dog","monkey"]
+# if g[3] == "lion":
+#     print("there is lion on index 3")
+# else:
+#     print("there is no lion on index 3")
+
+
+# basket = ["ვაშლი", "ბანანი", "საზამთრო", "ატამი", "ყურძენი"]
+# # print(basket[0])
+# # print(basket[2])
+# basket[1]="ალუბალი"
+# i=0
+# while i < 5:
+#     print(basket[i])
+#     i+=1
+
+
+# letters = ["ა", "ბ", "გ", "ო", "ლ", "ა", "მ", "ა", "ტ", "ე"]
+# print(letters[2])
+# print(letters[4] , letters[5])
+# print(letters[9])
+# print(letters[6] + letters[0] + letters[6] + letters[0])
+# print(letters[2] + letters[3] + letters[4] + letters[0])
+# print(letters[2] + letters[3] + letters[2] + letters[0])
+
+
+# letters = ["ა", "ბ", "გ", "ო", "ლ", "ა", "მ", "ა", "ტ", "ე"]
+# h=letters[4]
+# if h=="ლ":
+#     print("სწორია! ასო ლ ა")
+# else:
+#     print("არასწორია, სცადე თავიდან")
+
+# i=letters[9]
+# if i == "ე":
+#     print("საიდუმლო სიტყვა იწყება სწორად")
+# else:
+#     print("საიდუმლო სიტყვა არასწორია")
+
+# j=(letters[2] + letters[9] + letters[4] + letters[0])
+# if j=="გელა":
+#     print("გაარტყი სახელი!")
+# else:
+#     print("შტერი ხარ!დ")
+
+
+# k=["apple","pineapple","watermelon","banana",]
+# l=int(input("enter number between 0 to 3:"))
+# if l == 0 or l == 1 or l == 2 or l == 3:
+#     print(k[l])
+# else:
+#     print("thats not between 0 to 3")
