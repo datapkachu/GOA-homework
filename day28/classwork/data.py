0678866eb96ef904abdef1e8bf50740c0debcec3
datapkachu <makasakandelidze@gmail.com>
2025-10-09 22:31:26 +0400

    1

--- day28/classwork/data.py
+++ day28/classwork/data.py
@@ -1,29 +1,11 @@
-# a=[1,2,3,4,5,6,7,8,9,10]
-# print(a[9])
-# print(a[8])
-# print(a[7])
-# print(a[6])
-# print(a[5])
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a[5:])
 
 
-# b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-# print(b[-20])
-# print(b[-19])
-# print(b[-18])
-# print(b[-17])
-# print(b[-16])
-# print(b[-15])
-# print(b[-14])
-# print(b[-13])
-# print(b[-12])
-# print(b[-11])
-# print(b[-10])
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(b[-20:-9])
 
 
 c=[1,2,3,4,5,6,7,8,9,10,11,12]
-d=input("pick a number between 0 to 11 :")
-i=0
-while i != d:
-    print(d[1 + i])
-    i = i + 1
-    break
+d=input("enter a number between 0 to 12 :")
+print(c[2:int(d)])
