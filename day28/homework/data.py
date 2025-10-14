2cccca100a5abba0f0fb3293c995be51ab3d9bfc
datapkachu <makasakandelidze@gmail.com>
2025-10-14 17:34:11 +0400

    1

--- day28/homework/data.py
+++ day28/homework/data.py
@@ -1,13 +1,13 @@
 a=[1,2,3,4,5,6,7,8,9,10]
 print(a[:5])
-print(a[8:])
+print(a[7:])
 print(a[3:8])
 
 
-b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-print(b[-20:-14])
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(b[-15:-9])
 print(b[-10:])
-print(b[-14:-6])
+print(b[-8:])
 
 
 c="თვითმფრინავი"
@@ -28,10 +28,10 @@ print(e[5:])
 
 
 f="Python slicing is very powerful"
-print(f[:7])
+print(f[:6])
 print(f[7:14])
 print(f[23:])
 
 print(f[-31:-25])
-print(f[-24:-16])
+print(f[-24:-17])
 print(f[-8:])
--- day29/homework/data.py
+++ day29/homework/data.py
@@ -3,13 +3,7 @@ print(a[:6])
 
 
 numbers = [10, 20, 30, 40, 50, 60, 70]
-print(numbers[2])
-print(numbers[3])
-print(numbers[4])
-
-print(numbers[-5])
-print(numbers[-4])
-print(numbers[-3])
+print(numbers[-5:-2])
 
 
 b="HelloWorld"
