23de6815467acdd24ba8e226a17dab4915d322c6
datapkachu <makasakandelidze@gmail.com>
2025-10-18 15:42:04 +0400

    1

---
+++ day31/classwork/data.py
@@ -0,0 +1,12 @@
+a=["ina" , "givi" , "nika" , "daviti" , "ia" , "lizi"]
+a[:2]=["irina" , "milana" , "kira", "mate"]
+a[-2:]=["gia" , "emzari" , "xvicha"]
+print(a)
+
+
+
+b=input("enter any number:")
+if int(b) % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
