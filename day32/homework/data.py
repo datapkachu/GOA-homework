355f98f4f86a898f1ceba40bca73660801dd2bec
datapkachu <makasakandelidze@gmail.com>
2025-10-21 17:57:45 +0400

    1

--- day32/homework/data.py
+++ day32/homework/data.py
@@ -1,46 +1,65 @@
-# for i in range(1,50):
-#     if i % 2 == 0:
-#         print(f"ლუწი {i}")
-#     elif i % 2 == 1:
-#         print(f"კენტი {i}")
-#     elif i == 0:
-#         print(f"ნული {i}")
+for i in range(1,50):
+    if i % 2 == 0:
+        print(f"ლუწი {i}")
+    elif i % 2 == 1:
+        print(f"კენტი {i}")
+    elif i == 0:
+        print(f"ნული {i}")
 
 
-# for i in range(20):
-#     if i % 3 == 0:
-#         print(f"იყოფა 3-ზე {i}")
-#     elif i % 5 == 1:
-#         print(f"იყოფა 5-ზე{i}")
-#     elif i == 15:
-#         print(f"იყოფა 3-ზე და 5-ზე{i}")
-#     else:
-#         print(f"არ იყოფა არცერთზე {i}")
+for i in range(20):
+    if i % 3 == 0:
+        print(f"იყოფა 3-ზე {i}")
+    elif i % 5 == 0:
+        print(f"იყოფა 5-ზე{i}")
+    elif i % 15 == 0:
+        print(f"იყოფა 3-ზე და 5-ზე{i}")
+    else:
+        print(f"არ იყოფა არცერთზე {i}")
 
 
-# a=[10, 25, 33, 47, 80, 99]
-# for i in range(6):
-#     if a[i] > 50:
-#         print("მეტი 50-ზე")
-#     else:
-#         print("ნაკლები 50-ზე")
+a=[10, 25, 33, 47, 80, 99]
+for i in range(6):
+    if a[i] > 50:
+        print("მეტი 50-ზე")
+    elif a[i] <50:
+        print("ნაკლები 50-ზე")
 
 
-# for i in range(100):
-#     if i % 2 == 0:
-#         print(i)
-# print("ლუწი რიცხვების ჯამია: 2450")
+sum=0
+for i in range(100):
+    if i % 2 == 0:
+        print(i)
+        sum += i
+print(f"ლუწი რიცხვების ჯამია: {sum}")
 
 
-# for i in range(20):
-#     if i % 2 == 0:
-#         print("ლუწი")
-#     elif i % 2 == 1:
-#         print("კენტი")
-#     elif i == 0:
-#         print("ნული")
+c=["avarage","cool","under","apple"]
+for i in c:
+    if i[0] == "a":
+        print(i)
 
 
-# b=[5, 15, 25, 35, 45, 55]
-# for i in range(6):
-#     print(b[i])
+for i in range(20):
+    if i % 2 == 0:
+        print("ლუწი")
+    elif i % 2 == 1:
+        print("კენტი")
+    elif i == 0:
+        print("ნული")
+
+
+b=[5, 15, 25, 35, 45, 55]
+for i in range(6):
+    print(b[i])
+
+
+c = input("eneter any word:")
+for i in c:
+    print(i)
+
+
+total = 0
+for i in range(1,10):
+    total += i
+print(f"ჯამი არის: {total}")
