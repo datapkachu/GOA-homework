1f0848f9c1fb19efc567f52dee3efb36e16f2b3a
datapkachu <makasakandelidze@gmail.com>
2025-10-28 15:31:16 +0400

    1

---
+++ day34/classwork/data.py
@@ -0,0 +1,13 @@
+# a = input("enter any word :")
+# print(len(a))
+# for i in range(len(a)):
+#     print(a[i])
+
+
+b = ["data","cool","handsome","deme","uncool","ugly"]
+for i in range(len(b)):
+    print(len(b[i]))
+    if len(b[i]) % 2 == 0:
+        print("ლუწი")
+    else:
+        print("კენტი")
