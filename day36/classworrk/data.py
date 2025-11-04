d8320f3bc1c79d372e9f564161cc93da3f990d70
datapkachu <makasakandelidze@gmail.com>
2025-11-04 14:23:37 +0400

    1

---
+++ day36/classworrk/data.py
@@ -0,0 +1,10 @@
+a = input("type in any word :")
+print("a" in a or "A" in a)
+print("car" not in a)
+
+
+b = input("enter anything :")
+for i in b:
+    if i == "a" or i == "A":
+        continue
+    print(i)
