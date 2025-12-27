d3d689e032ac68dff9be958e354e43f40b9573a0
datapkachu <makasakandelidze@gmail.com>
2025-12-27 21:09:54 +0400

    1

--- day45/homework/data.py
+++ day45/homework/data.py
@@ -20,12 +20,27 @@
 #         e.remove(i)
 # print(e)
 
-e1 = ["data" , "nika" , "goga" , "giorgi" , "levani" , "gogi"]
-g = -1
-for i in e1:
-    g += 1
-    if "g" in i[0] and "i" in i[-1]:
-        e1.pop(g)
-print(e1)
+# e1 = ["data" , "nika" , "goga" , "giorgi" , "levani" , "gogi"]
+# g = 0
+# for i in e1:
+#     g += 1
+#     if "g" in i[0] and "i" in i[-1]:
+#         e1.pop(g-1)
+# print(e1)
 
 
+# k = ["data" , "nika" , "GOGA" , "giorgi" , "levani" , "GOGI"]
+# for i in k:
+#     if "G" in i[0] and i[-2] == i[-2].upper() and i[-1] == i[-1].upper():
+#         k.remove(i)
+# for i in range(len(k)):
+#     k[i]= k[i].upper()
+# print(k)
+
+
+l = [1,2,3,4,5,6,7,8,9]
+m = ["data" , "nika" , "GOGA" , "giorgi" , "levani" , "GOGI"]
+for i in l:
+    if i % 2 == 0 and i != 0:
+        l.remove(i)
+print(l)
